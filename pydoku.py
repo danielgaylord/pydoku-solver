fbb8f492bfed812a95c34f2523707161e9dce297
Daniel Gaylord <danielgaylord@gmail.com>
2021-11-09 22:30:14 -0500

    major updates
    re did flask. added react. added heroku

--- pydoku.py
+++ pydoku.py
@@ -1,1 +1,14 @@
-from app import app
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+#from flask_cors import CORS #comment on deployment
+from backend.brains import Brains
+
+app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+#CORS(app) #comment on deployment
+api = Api(app)
+
+@app.route("/", defaults={'path':''})
+def serve(path):
+    return send_from_directory(app.static_folder,'index.html')
+
+api.add_resource(Brains, '/flask/hello')
