9f081374e7f9300abd169d63a7ad24f968f482a9
Daniel Gaylord <danielgaylord@gmail.com>
2022-01-17 11:04:11 -0500

    Update cell.py

--- cell.py
+++ cell.py
@@ -4,16 +4,16 @@ class Cell():
     # init/constructor
     def __init__(self, value):
         self.value = value
-        self.options = []
+        self.options = set()
 
     # determine the potential values based on the size of the region
     def set_options(self, region_size):
         if self.value == 0:
-            self.options = [x for x in range(1, region_size + 1) if self.value == 0]
+            self.options = {val for val in range(1, region_size + 1)}
 
 # For testing purposes
 # if __name__ == "__main__":
-#     test = Cell(1)
+#     test = Cell(0)
 #     print("Value:", test.value)
 #     test.set_options(9)
 #     print("Options:", test.options)
