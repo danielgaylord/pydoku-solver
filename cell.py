d92e0cc6436505613848850c263776654a19e79a
Dan Gaylord <danielgaylord@gmail.com>
2022-01-31 21:56:43 -0500

    Revert "Update cell.py"
    This reverts commit 9f081374e7f9300abd169d63a7ad24f968f482a9.

--- cell.py
+++ cell.py
@@ -4,16 +4,16 @@ class Cell():
     # init/constructor
     def __init__(self, value):
         self.value = value
-        self.options = set()
+        self.options = []
 
     # determine the potential values based on the size of the region
     def set_options(self, region_size):
         if self.value == 0:
-            self.options = {val for val in range(1, region_size + 1)}
+            self.options = [x for x in range(1, region_size + 1) if self.value == 0]
 
 # For testing purposes
 # if __name__ == "__main__":
-#     test = Cell(0)
+#     test = Cell(1)
 #     print("Value:", test.value)
 #     test.set_options(9)
 #     print("Options:", test.options)
